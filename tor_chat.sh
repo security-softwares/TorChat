9ede70a6e7b47c17a31e9461a94bf29f02914825
hacker X <110929453+security-softwares@users.noreply.github.com>
2022-10-15 09:45:16 +0530

    Update tor_chat.sh

--- tor_chat.sh
+++ tor_chat.sh
@@ -16,7 +16,7 @@ then
 service tor start
 read -p "enter port " port
 echo -e "HiddenServiceDir /var/lib/tor/hidden_service/ \n HiddenServicePort 80 127.0.0.1:$port" >> /etc/tor/torrc
-echo "link to join-;"
+echo "link to join-:"
 cat /var/lib/tor/hidden_service/hostname
 echo -e "server should be host on port 8080 and join port 80\n"
 torsocks python3 /etc/TorChat/chat_server.py
