1bdc36cdb7bf9a8259dc4754eaa2c741d9c3359e
hacker X <110929453+security-softwares@users.noreply.github.com>
2022-10-16 18:06:16 +0530

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,10 +1,6 @@
-if [ "$EUID" -ne 0 ]
-then
-echo "run with sudo"
-exit
-fi
+
 echo -e "setting up"
-chmod u+x tor_chat.sh
+chmod +x tor_chat.sh
 cd ..
 cp -r TorChat /etc/
 
@@ -12,4 +8,5 @@ cp -r TorChat /etc/
 cd TorChat
 sudo ln -s /etc/TorChat/tor_chat.sh /usr/local/bin/tor_chat
 sudo ln -s /etc/TorChat/tor_chat.sh /usr/bin/tor_chat
-
+#sudo cp tor_chat.sh /usr/bin
+#sudo chmod 744 /usr/local/bin/tor_chat
