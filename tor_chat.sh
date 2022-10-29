b2cbe48ea37745add95674e2eaaa6ca3b1eb3521
hacker X <110929453+security-softwares@users.noreply.github.com>
2022-10-29 19:04:33 +0530

    Update tor_chat.sh

--- tor_chat.sh
+++ tor_chat.sh
@@ -1,10 +1,16 @@
 #!/bin/bash
+
+
+
 if [ -z "$1" ]
 then
 echo -e "\033[1;32m Tor_Chat is encrypted chat over Tor service \n for more visit :\033[0m https://github.com/security-softwares"
 echo -e "\033[1;33musage: 	sudo tor_chat -s : For server side \n	sudo tor_chat -c : For client side \033[0m \n 	tor_chat -u: uninstall"
 exit
 fi
+
+
+
 
 if [ "$EUID" -ne 0 ]
 then
@@ -13,7 +19,9 @@ exit
 elif [ "$1" == "-s" ]
 
 then
+
 sudo cat torrc > /etc/tor/torrc
+trap 'cat Normal_Torrc >/etc/tor/torrc' 1 2/3 9
 service tor start 2>/dev/null
 echo "link to join-:"
 cat /var/lib/tor/hidden_service/hostname
@@ -22,6 +30,7 @@ torsocks python3 chat_server.py
 
 elif [ "$1" == "-c" ]
 then 
+
 service tor start 2>/dev/null
 torsocks python3 chat_client.py
 
